38de88da18f52afb38a4b436e5381ac331f5fc0f
Atheer Abdullah <Atheer98.abdullah19@gmail.com>
2023-08-15 01:24:53 +0300

    lab

--- Dj/main/urls.py
+++ Dj/main/urls.py
@@ -5,12 +5,12 @@ app_name = 'Dj_app'
 
 urlpatterns = [
     
-    path('',views.home),
-    path('today/',views.today),
-    path('random/',views.random),
-    path('random/password/',views.password),
-    path('favs/',views.favs),
-    path('favs/games/',views.games),
+    path('',views.home , name = "home"),
+    path('today/',views.today, name= "today"),
+    path('random/',views.random, name= "random"),
+    path('random/password/',views.password, name= "password"),
+    path('favs/',views.favs,name= "favs"),
+    path('favs/games/',views.games,name= "games"),
 
 
 ]
--- Dj/main/views.py
+++ Dj/main/views.py
@@ -25,7 +25,8 @@ def random(request: HttpRequest):
 
 def password(request: HttpRequest):
     
-    password = "555555"
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(8))
 
     context = {"password": password}
 
